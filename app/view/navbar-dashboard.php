412a2760972c8e109842ca33e6f282537b215879
Asep Ama Rosmana <asepamarosmana@gmail.com>
2025-09-27 22:43:39 +0700

    Update navbar-dashboard.php

--- app/view/navbar-dashboard.php
+++ app/view/navbar-dashboard.php
@@ -1,5 +1,5 @@
 <div class="navbar">
-        <img src="/project-web-gdsc/app/view/assets/image/campusebooklogo.png" alt="logo kampus book">
+        <img src="/app/view/assets/image/campusebooklogo.png" alt="logo kampus book">
     <div class="navbar-search">
             <form action="" method="get">
                 <input type="text" name="search" placeholder="Search">
