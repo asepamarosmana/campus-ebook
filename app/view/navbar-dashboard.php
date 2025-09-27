fd84ab298224a265b3bc9ac469ae9fb71bd002bb
Asep Ama Rosmana <asepamarosmana@gmail.com>
2025-09-27 22:22:40 +0700

    Delete app directory

--- app/view/navbar-dashboard.php
+++
@@ -1,8 +0,0 @@
-<div class="navbar">
-        <img src="/app/view/assets/image/campusebooklogo.png" alt="logo kampus book">
-    <div class="navbar-search">
-            <form action="" method="get">
-                <input type="text" name="search" placeholder="Search">
-            </form>
-    </div>
-</div>
